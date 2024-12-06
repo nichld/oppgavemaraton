999734c6a19f9df4e050f67195329ad16cfd8ca9
Nichlas <141312928+nichld@users.noreply.github.com>
2024-12-06 13:02:27 +0100

    Added mongoDB from task 7

---
+++ models/Message.js
@@ -0,0 +1,8 @@
+// models/Message.js
+const mongoose = require('mongoose');
+
+const messageSchema = new mongoose.Schema({
+    message: String
+});
+
+module.exports = mongoose.model('Message', messageSchema, 'greetings');
