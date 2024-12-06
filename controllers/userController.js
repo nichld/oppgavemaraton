d22a43da355ee4d7ca0d7fdb3a306b9c30d69ff8
Nichlas <141312928+nichld@users.noreply.github.com>
2024-12-06 14:07:29 +0100

    Added stuff from task 9

--- controllers/userController.js
+++ controllers/userController.js
@@ -6,7 +6,8 @@ exports.login = async (req, res) => {
   try {
     const user = await User.findOne({ username });
     if (user && await bcrypt.compare(password, user.password)) {
-      res.send('Login successful');
+      req.session.user = user;
+      res.redirect('/galleri');
     } else {
       res.send('Invalid username or password');
     }
@@ -21,7 +22,8 @@ exports.register = async (req, res) => {
     const hashedPassword = await bcrypt.hash(password, 10);
     const newUser = new User({ username, password: hashedPassword });
     await newUser.save();
-    res.send('User registered successfully');
+    req.session.user = newUser;
+    res.redirect('/galleri');
   } catch (error) {
     res.status(500).send('Error registering user');
   }
